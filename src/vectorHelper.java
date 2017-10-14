25b2ef5f04cb66c9e834f620563822d558504710
JUV <ey_al_houcine@esi.dz>
2017-10-14 17:25:40 +0100

    j'ai créé tous les arguments

--- src/vectorHelper.java
+++ src/vectorHelper.java
@@ -1,12 +1,23 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.util.Scanner;
+
 /**
  * Created by JUV on 14/10/2017.
  */
 public class vectorHelper {
-    // initial commit
+    public int max = 1000;
+    public int nbElements = 0;
+    public int nbElements2= 0;
+    public int nbElements3= 0;
+    public int intTab[] = new int[max];
+    public int intTab2[] = new int[max];
+    public int intTab3[] = new int[max];
+    public int mintab,maxtab;
+    Scanner sc = new Scanner(System.in);
     public static void main (String args[]){
         System.out.print("hello world");
+
 
     }
 }
